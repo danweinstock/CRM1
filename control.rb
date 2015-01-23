929be84e0ca8c99a8411962d5a89e2c5e1826fa8
danweinstock <dan.weinstock@yahoo.com>
2015-01-23 14:09:44 -0500

    CRM files

---
+++ control.rb
@@ -0,0 +1,79 @@
+#iterators
+
+#.times 
+
+# 3.times { |counter| puts "Yummy!: #{counter}"}
+
+# 3.times do |counter|
+# 	puts "Grrr!: #{counter}"
+# end
+
+one_to_ten = (1..10)
+
+one_to_ten.each do |number|
+	puts (number**2)
+end
+# loops
+
+ # while loops
+#  counter = 1
+
+#  while counter <=10 
+#  	puts "Counter is at : #{counter}"
+# counter +=1
+#  end
+
+#until
+# counter =1
+
+# until counter == 11
+# 	puts "Counter is at  #{counter}"
+# 	counter +=1
+# end
+
+# #inline if
+# x = 12
+# puts "I get printed" if x!=10
+
+# puts "I still get printed" unless x == 10
+
+# # unless
+# x=9
+# if x!=10
+# 	puts "I get printed"
+# end
+
+# unless x ==10
+# 	puts "I still get printed"
+# end
+# no unless-else!!
+# nu unless not
+
+
+# else
+# number = -5
+# if number > 0
+# 	puts "#{number} is positive"
+# else
+# 	puts "#{number} is negative"
+# end
+
+
+# elseif
+# x=3
+# y=6
+# if x > y
+# 	puts "x is greater than y"
+# elsif x < y
+# 	puts "x is less than y"
+# else
+# 	puts "x is equal to y"
+# end
+
+
+# If
+# my_var =5
+
+# if my_var<=4
+# 	puts "I get printed"
+# end
